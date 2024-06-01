afc6f3f42218b8c0c630d5eca19ff60e3dd4aac0
Tigran <tigra.azaryan@me.com>
2024-05-31 19:28:23 -0700

    Add lesson2 files

--- lesson2/files.py
+++ lesson2/files.py
@@ -1,3 +1,15 @@
-fw = open('doc/file.txt', 'a')
-fw.write("Hello world")
+# a - запись новых данных в файл и помещение их в конец файла
+fw = open('doc/new.txt', 'a')
+fw.write("Here I am entering some text\n")
 fw.close()
+
+# w - запись новых данных в файл и удаление старых данных
+fw = open('doc/new_2.txt', 'w')
+fw.write("Here I am entering some new text to make sure that it deletes an old text\n")
+fw.close()
+
+fr = open('doc/new_2.txt', 'r')
+text = fr.read()
+fr.close()
+
+print(text)
